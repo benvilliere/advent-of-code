6edffe63b28160cf41289e4097d94e1f2846c54b
notu <notuart@pm.me>
2023-12-03 21:57:29 +0000

    Advent of Code - Day 01

--- day-01/part-01.js
+++ day-01/part-01.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const file = './input.txt';
-const puzzle = fs.readFileSync(file, 'utf-8');
+const file = "./input.txt";
+const puzzle = fs.readFileSync(file, "utf-8");
 
 const solution = puzzle
-  .split('\n')
-  .map((line) => line.split('').filter((char) => !isNaN(char)))
+  .split("\n")
+  .map((line) => line.split("").filter((char) => !isNaN(char)))
   .filter((digits) => digits.length > 0)
   .map((digits) => {
     const first = digits.length > 0 ? digits[0] : 0;
@@ -15,4 +15,4 @@ const solution = puzzle
   })
   .reduce((prev, current) => prev + current);
 
-console.log(solution);
+console.log(solution); // 55017
